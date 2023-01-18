c5398612f65ea13d4ab48ceffa9c37bc6c19b86c
Jordi-Allepuz <valencianu@gmail.com>
2023-01-18 12:26:15 +0100

    Commit 3

---
+++ src/Enemigo.java
@@ -0,0 +1,39 @@
+public class Enemigo {
+
+    private static String[] enemigos = {"Enemigo1", "Enemigo2", "Enemigo3"};
+    private String type;
+    private int damage;
+    private int life;
+
+
+    public Enemigo() {
+        int numEnemy = (int) (Math.random() * 3) + 1;
+
+        switch (numEnemy) {
+            case 1:
+                this.type = "Araña gigante";
+                this.life = 50;
+                this.damage = 3;
+                break;
+            case 2:
+                this.type = "Caballero oscuro";
+                this.life = 80;
+                this.damage = 8;
+                break;
+            case 3:
+                this.type = "Espectro";
+                this.life = 30;
+                this.damage = 2;
+                break;
+            default:
+        }
+
+    }
+
+
+    public String toString() {
+
+        return  "Tipo: " + type + "\n" + "Daño: " + damage + "\n" + "Vida: " + life;
+
+
+    }
