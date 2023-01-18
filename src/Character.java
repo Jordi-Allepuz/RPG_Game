236a7402c3d2c90c7a3936017d33c86e0443d013
Jordi-Allepuz <valencianu@gmail.com>
2023-01-18 12:15:44 +0100

    Commit 2

--- src/Character.java
+++ src/Character.java
@@ -1,31 +1,53 @@
+import java.util.Scanner;
+
 public class Character {
 
     private String type;
-    private static String[] typeList = {"Arquero", "Mago", "Caballero"}
+    private static String[] typeList = {"Arquero", "Mago", "Caballero"};
     private int damage;
     private int life;
     private String name;
 
 
-    public Character(String name, String type) {
-        this.name = name;
-        this.type = type;
+    public Character() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Menu de personaje");
+        System.out.print("Escribe un nombre: ");
+        this.name= sc.nextLine();
+        System.out.println("Elige personaje");
+        System.out.println("Pulsa 1 para Aquero");
+        System.out.println("Pulsa 2 para Mago");
+        System.out.println("Pulsa 3 para Caballero");
+        int personaje=sc.nextInt();
 
-        switch (type) {
-            case "Arquero":
+        switch (personaje) {
+            case 1:
+                this.type="Arquero";
                 this.life = 100;
                 this.damage = 5;
                 break;
-            case "Mago":
+            case 2:
+                this.type="Mago";
                 this.life = 80;
                 this.damage = 15;
                 break;
-            case "Caballero":
+            case 3:
+                this.type="Caballero";
                 this.life = 120;
                 this.damage = 10;
                 break;
             default:
         }
+    }
+
+
+    public String toString(){
+
+        return "Nombre: " + name + "\n" + "Tipo: " + type
+                + "\n" + "Daño: " + damage + "\n" +
+                "Vida: " + life;
+
+
     }
 
 
---
+++ src/VIdeogame.java
@@ -0,0 +1,10 @@
+public class VIdeogame {
+
+    public static void main(String[] args) {
+
+
+        Character personaje1= new Character();
+        System.out.println(personaje1);
+
+    }
+}
